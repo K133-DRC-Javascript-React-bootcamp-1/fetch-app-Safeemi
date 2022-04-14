c9dbc8914340debbf24d76d927a277a30f57d935
Safeemi <98190229+Safeemi@users.noreply.github.com>
2022-04-14 20:29:06 +0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,25 @@
+const theInput = document.querySelector(".get-repos input");
+const getButton = document.querySelector(".get-button");
+const reposData = document.querySelector(".show-data");
+
+getButton.onclick = function () {
+  getRepos();
+};
+// Get repos function
+
+function getRepos() {
+  if (theInput.value == "") {
+    reposData.innerHTML = "<span> Please Write Github Usename </span>";
+  } else {
+    let userListDom = document.querySelector("#userList");
+    fetch(`https://randomuser.me/api/?results=150`)
+      .then((response) => response.json())
+      .then((response) => console.log(response));
+    let results = responseJson.results;
+    results.forEach((item) => {
+      let liDOM = document.createElement("li");
+      liDOM.innerHTML = item.age;
+      userListDom.appendChild(liDOM);
+    });
+  }
+}
